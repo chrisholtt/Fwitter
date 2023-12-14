9eafa52a8bcd8de1668bc79e82b2fc9e8ea749eb
Chris <chrisphilholt@gmail.com>
2023-12-14 00:37:36 +0000

    trying to builkd

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -3,31 +3,31 @@ import MessagesFeed from "@/components/messages/MessagesFeed";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
 
-  console.log(session)
-  console.log(session)
-  console.log(session)
-  if (!session) {
-    console.log("no session")
-    console.log("no session")
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      }
-    }
-  }
+//   console.log(session)
+//   console.log(session)
+//   console.log(session)
+//   if (!session) {
+//     console.log("no session")
+//     console.log("no session")
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       }
+//     }
+//   }
 
-  return {
-    props: {
-      session
-    }
-  }
-}
+//   return {
+//     props: {
+//       session
+//     }
+//   }
+// }
 
-console.log(2)
+// console.log(2)
 
 
 const Notifications = () => {
